70226e3e459c5a8291552850d0e12c28e96eeca9
jvonappen <99298095+jvonappen@users.noreply.github.com>
2023-12-18 19:06:41 +1030

    tp script

---
+++ Assets/_Scripts/TP4BungHole.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TP4BungHole : MonoBehaviour
+{
+    [SerializeField] GameObject _tp1;
+    [SerializeField] GameObject _tp2;
+    [SerializeField] GameObject _tp3;
+    [SerializeField] GameObject _tp4;
+
+}
