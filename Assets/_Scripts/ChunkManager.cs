1825f124d3d13b618029d5c53b251f9712c4abc5
Logan Baker <loganpbaker05@gmail.com>
2023-12-20 02:00:38 +1030

    I dont want to talk about it

--- Assets/_Scripts/ChunkManager.cs
+++ Assets/_Scripts/ChunkManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using static UnityEngine.ParticleSystem;
 
 [System.Serializable]
 public struct ChunkSpawn
