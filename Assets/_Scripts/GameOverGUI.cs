54850d1f7c332d0c4548ebf7dbef3c8d56934049
jvonappen <99298095+jvonappen@users.noreply.github.com>
2023-12-19 12:06:45 +1030

    Runner script tweaks

--- Assets/_Scripts/GameOverGUI.cs
+++ Assets/_Scripts/GameOverGUI.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEditor.SearchService;
+using UnityEngine.SceneManagement;
 
 public class GameOverGUI : MonoBehaviour
 {
@@ -9,30 +11,50 @@ public class GameOverGUI : MonoBehaviour
     [SerializeField] GameObject _winImage;
     [SerializeField] GameObject _failImage;
     [SerializeField] TextMeshProUGUI _scoreText;
-    [SerializeField] TextMeshProUGUI _hiScoreText;
+    [SerializeField] TextMeshProUGUI _highScoreText;
 
     [Header("Debug")]
     [SerializeField] int _score;
-    [SerializeField] int _hiScore;
+    [SerializeField] int _highScore;
+    public static GameOverGUI Instance;
 
-    public void DisplayGameOverGUI()
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void DisplayGameOverGUI(int currentScore, int highScore)
     {
         Debug.Log("Allo there");
 
         _canvas.enabled = true;
 
-        _scoreText.text = _score.ToString();
-        _hiScoreText.text = _hiScore.ToString();
+        _scoreText.text = currentScore.ToString();
+        _highScoreText.text = highScore.ToString();
 
-        if( _score > _hiScore )
+        if(currentScore > highScore)
         {
             _winImage.SetActive(true);
             _failImage.SetActive(false);
         }
-        else if(_score <= _hiScore )
+        else if(currentScore <= highScore)
         {
             _winImage.SetActive(false);
             _failImage.SetActive(true);
         }
+    }
+
+    public void RestartGame()
+    {
+        Debug.Log("Reloadedededed");
+        SceneManager.LoadScene(1);
     }
 }
